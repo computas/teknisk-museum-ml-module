ab61e8ebea24dbae543b89559510636321fd506b
joaaax <joaax@computas.com>
2020-07-13 15:42:21 +0200

    Add skeleton code for ml data class

---
+++ src/ml_data.py
@@ -0,0 +1,28 @@
+"""
+    This file contains the Data class, which is responsible for handling
+    and processing the data used for training and testing the Machine
+    Learning Model. The class should have a function which separates the
+    data in to training and test data.
+"""
+import config as cfg
+
+
+class Data():
+    """
+        This class represents the overall data used for both training
+        and testing the Machine Learning Model. The goal is to make it
+        easy to retrieve the data and to separate it into test and
+        training data for the Model class.
+    """
+    def __init__(self):
+        """
+            Constructor for the class.
+        """
+        pass
+    
+
+    def load(self):
+        """
+            Retrieve the data to the caller as test and training data.
+        """
+        pass
